4f3b5c18b8160a242bd39d0459249c1878477d9c
manav <manavsharma@finnaux.co.in>
2025-07-22 15:20:41 +0530

    updated finnaux get api

--- src/routes/finnaux/index.ts
+++ src/routes/finnaux/index.ts
@@ -1,14 +1,13 @@
 import { db } from "@/db";
+import { finnauxData } from "@/db/schemas/finnaux";
 import { longFormTable, LongFormTableType } from "@/db/schemas/long-form";
 import { yupValidator } from "@/lib/yup/validator";
-import { and, eq, gte, lte } from "drizzle-orm";
+import { filePathArrayToBase64, filePathToBase64 } from "@/utils";
+import { and, eq, getTableColumns, gte, lte } from "drizzle-orm";
 import { Hono } from "hono";
 import HttpStatus from "http-status";
-import { getLongFormData, getLongFormDataType, putLongFormData, putLongFormDataType } from "./schema";
-import { finnauxData } from "@/db/schemas/finnaux";
-import { filePathArrayToBase64, filePathToBase64 } from "@/utils";
 import { sendConfirmationEmail } from "../long-form/services";
-import { sendEmailOtp } from "@/verification-service";
+import { getLongFormData, getLongFormDataType, putLongFormData, putLongFormDataType } from "./schema";
 
 type DocumentType = string[] | null
 
@@ -39,34 +38,40 @@ app.get(
   async (c) => {
     const query: getLongFormDataType = c.req.valid("query");
 
-    const data: LongFormTableType[] = await db
-      .select()
+    const data: any[] = await db
+      .select({
+        ...getTableColumns(finnauxData),
+        ...getTableColumns(longFormTable),
+      })
       .from(longFormTable)
+      .leftJoin(finnauxData, eq(finnauxData.id, '62dcd4ea-1c98-40d7-a228-aba4c5c051a5'))
       .where(
         and(
-          lte(longFormTable.createdAt, new Date(query.to).toISOString()),
-          gte(longFormTable.createdAt, new Date(query.from).toISOString())
+          // lte(longFormTable.createdAt, new Date(query.to).toISOString()),
+          // gte(longFormTable.createdAt, new Date(query.from).toISOString()),
+          eq(longFormTable.id, '62dcd4ea-1c98-40d7-a228-aba4c5c051a5')
         )
       )
+    // .leftJoin(longFormTable, eq(finnauxData.id, '62dcd4ea-1c98-40d7-a228-aba4c5c051a5'))
 
-    const updatedData = await Promise.all(data.map(
-      async (data) => {
-        const updatedData: UpdatedLongFormType = {
-          ...data,
-          profilePicture: await filePathToBase64(data.profilePicture),
-          aadhaarFront: await filePathToBase64(data.aadhaarFront),
-          aadhaarBack: await filePathToBase64(data.aadhaarBack),
-          panCard: await filePathToBase64(data.panCard),
-          salarySlips: await filePathArrayToBase64(data.salarySlips),
-          employmentProofDocument: await filePathToBase64(data.employmentProofDocument),
-          bankStatement: await filePathToBase64(data.bankStatement),
-        }
+    // const updatedData = await Promise.all(data.map(
+    //   async (data) => {
+    //     const updatedData: UpdatedLongFormType = {
+    //       ...data,
+    //       profilePicture: await filePathToBase64(data.profilePicture),
+    //       aadhaarFront: await filePathToBase64(data.aadhaarFront),
+    //       aadhaarBack: await filePathToBase64(data.aadhaarBack),
+    //       panCard: await filePathToBase64(data.panCard),
+    //       salarySlips: await filePathArrayToBase64(data.salarySlips),
+    //       employmentProofDocument: await filePathToBase64(data.employmentProofDocument),
+    //       bankStatement: await filePathToBase64(data.bankStatement),
+    //     }
 
-        return updatedData
-      }
-    ))
+    //     return updatedData
+    //   }
+    // ))
 
-    return c.json({ data: updatedData }, HttpStatus.OK)
+    return c.json({ data }, HttpStatus.OK)
   }
 )
 
